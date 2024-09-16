f0267b6047042f8f480f21535077b83ca7d21e54
unknown <150057130+AmoS2811@users.noreply.github.com>
2024-09-16 14:55:14 -0400

    Added backend controller and route for products

---
+++ server/dist/src/routes/productRoutes.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const productController_1 = require("../controllers/productController");
+const router = (0, express_1.Router)();
+router.get("/", productController_1.getProducts);
+router.post("/", productController_1.createProduct);
+/* TODO
+router.put("/", updateProduct);
+router.delete("/", deleteProduct);
+*/
+exports.default = router;
